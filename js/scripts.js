e98a9427429e918ce609b93a76bdb8eabcc4188d
Riley Benson <ryguybenson@yahoo.com>
2019-02-01 10:37:54 -0800

    change size to #size so size of pizza input is recognzied

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
-function Pizza(toppings,size) {
-  this.toppings = toppings;
+function Pizza(topping,size) {
+  this.topping = topping;
   this.size = size;
 };
 var price = 0;
-Pizza.prototype.pizzaPrice = function(inputtedSize, toppingResponses) {
-  if (inputtedSize === "small") {
+Pizza.prototype.pizzaPrice = function(size, toppingResponses) {
+  if (size === "small") {
     price += 8;
     console.log(price);
-  } else if (inputtedSize === "medium") {
+  } else if (size === "medium") {
     price += 12;
-  } else if (inputtedSize === "large") {
+  } else if (size === "large") {
     price += 16;
   }
   if (toppingResponses.includes("extraCheese")) {
-    price += 5;
+    price += 3;
   }
   if (toppingResponses.includes("bacon")) {
-    price += 1;
+    price += 2;
   }
    if (toppingResponses.includes("olives")) {
     price += 1;
@@ -35,8 +35,8 @@ console.log(price);
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var inputtedSize = $("size").val();
-      alert(inputtedSize);
+    var size = $("#size").val();
+      alert(size);
     var toppingResponses = [];
     $("input:checkbox[name=topping]:checked").each(function(){
       var inputtedToppings = $(this).val();
@@ -44,8 +44,8 @@ $(document).ready(function() {
       toppingResponses.push(inputtedToppings);
     });
     console.log(toppingResponses);
-    myPizza = new Pizza(inputtedSize,toppingResponses);
-    myPizza.pizzaPrice(inputtedSize, toppingResponses);
+    myPizza = new Pizza(size,toppingResponses);
+    myPizza.pizzaPrice(size, toppingResponses);
     console.log(price);
   });
 });
