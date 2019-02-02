fd78a1497d40f8905bc00c4f2178b4ffe4aeb08c
Riley Benson <ryguybenson@yahoo.com>
2019-02-01 17:20:51 -0800

    scripts work properly with object and display price

--- js/scripts.js
+++ js/scripts.js
@@ -2,48 +2,42 @@ function Pizza(toppingResponses, size) {
   this.toppingResponses = toppingResponses,
   this.size = size,
   this.price = 0;
-  console.log(this.toppingResponses);
   console.log(this.size);
 }
 
 Pizza.prototype.pizzaSize = function() {
-  if (this.toppingResponses === "small") {
+  if (this.size === "small") {
     this.price += 8;
- console.log(this.price);
-  } else if (this.toppingResponses === "medium") {
+  } else if (this.size === "medium") {
     this.price += 12;
-  } else if (this.toppingResponses === "large") {
+  } else if (this.size === "large") {
     this.price += 16;
   }
-  return (this.price);
 }
 //I AM SO CONFUSED WHY THIS.SIZE AND THIS.TOPPINGRESPONSES ARE BACKWARDS???????
 Pizza.prototype.pizzaToppings = function() {
-  if (this.size.includes("extra-cheese")) {
+  if (this.toppingResponses.includes("extra-cheese")) {
     this.price += 3;
   }
-  if (this.size.includes("bacon")) {
+  if (this.toppingResponses.includes("bacon")) {
     this.price += 2;
   }
-   if (this.size.includes("olive")) {
+   if (this.toppingResponses.includes("olive")) {
     this.price += 1;
   }
-   if (this.size.includes("pineapple")) {
+   if (this.toppingResponses.includes("pineapple")) {
     this.price += 1;
   }
-   if (this.size.includes("anchovy")) {
+   if (this.toppingResponses.includes("anchovy")) {
     this.price += 1;
   }
-   if (this.size.includes("pepperoni")) {
+   if (this.toppingResponses.includes("pepperoni")) {
     this.price += 1;
   }
-  return this.price;
-  console.log(this.price);
 }
 
 Pizza.prototype.totalPrice = function() {
-  return this.price;
-  console.log(this.price);
+  return "Your " + this.size + " " + this.toppingResponses + " pizza will cost " + this.price + " dollars"
 }
 
 $(document).ready(function() {
@@ -56,12 +50,11 @@ $(document).ready(function() {
       console.log(inputtedToppings);
       toppingResponses.push(inputtedToppings);
     });
-    var myPizza = new Pizza(size, toppingResponses);
+    var myPizza = new Pizza(toppingResponses, size);
     myPizza.pizzaToppings();
-    myPizza.totalPrice();
     myPizza.pizzaSize();
-    Pizza();
-    $("#ordered").text("Your " + size + " " + toppingResponses + " pizza will cost you " + this.price + " dollars");
+    myPizza.totalPrice();
+    $("#ordered").text(myPizza.totalPrice());
     this.price = 0;
   });
 });
